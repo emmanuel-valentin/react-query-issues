d450e93366d4ac59e3b1de8492500e236431caf5
Emmanuel Valentin <emmanuel-valentin@outlook.com>
2024-01-22 23:55:35 -0600

    Fin sección 5

--- src/issues/components/IssueItem.tsx
+++ src/issues/components/IssueItem.tsx
@@ -1,6 +1,8 @@
 import { FiInfo, FiMessageSquare, FiCheckCircle } from 'react-icons/fi';
+import { useQueryClient } from '@tanstack/react-query';
 import { Issue, State } from '../interfaces';
 import { useNavigate } from 'react-router-dom';
+import { getIssueComments, getIssueInfo } from '../hooks/useIssue';
 
 interface Props {
   issue: Issue;
@@ -10,8 +12,45 @@ export const IssueItem: React.FC<Props> = ({ issue }) => {
   const { title, state, user, number, comments } = issue;
   const navigate = useNavigate();
 
+  const queryClient = useQueryClient();
+
+  // Hace las peticiones http cuando pasa el mouse por el issue, sin embargo, puede disparar
+  // muchas peticiones, por lo que dependiendo del contexto de la aplicación, se puede
+  // configurar el staleTime
+
+  // const prefetchData = () => {
+  //   queryClient.prefetchQuery({
+  //     queryKey: ['issue', issue.number],
+  //     queryFn: () => getIssueInfo(issue.number),
+  //     staleTime: 1000 * 60 * 10,
+  //   });
+
+  //   // TODO: Cargar los comentarios,
+  //   queryClient.prefetchQuery({
+  //     queryKey: ['issue', issue.number, 'comments'],
+  //     queryFn: () => getIssueComments(issue.number),
+  //     staleTime: 1000 * 60 * 10,
+  //   });
+  // };
+
+  // Setea la data al issue antes de entrar a él, pero no dispara una petición http. Esto es posible porque al cargar las issues,
+  // la api responde la información suficiente como para verla detalladamente. Sin embargo, la data sí se establece en caché.
+  const preSetData = () => {
+    queryClient.setQueryData(
+      ['issue', issue.number],
+      issue,
+      // La data se mantiene fresh hasta la hora actual más un minuto
+      { updatedAt: new Date().getTime() + 100000 }
+    );
+  };
+
   return (
-    <div className="card mb-2 issue" onClick={() => navigate(`/issues/issue/${number}`)}>
+    <div
+      className="card mb-2 issue"
+      onClick={() => navigate(`/issues/issue/${number}`)}
+      // onMouseEnter={prefetchData}
+      onMouseEnter={preSetData}
+    >
       <div className="card-body d-flex align-items-center">
         <div className="img-fluid">
           {state === State.Open ? (
